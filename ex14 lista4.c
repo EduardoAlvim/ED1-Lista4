c6f5ccd6772c19a3fce0253799d73b4ba26fa7eb
EduardoAlvim <105020093+EduardoAlvim@users.noreply.github.com>
2022-06-03 11:50:40 -0300

    Add files via upload

---
+++ ex14 lista4.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void retornaFrase(char frase[], char frasef[]);
+
+void main()
+{
+    char frase[100], frasef[500];
+    printf("\nDigite sua frase: ");
+    gets(frase);
+    retornaFrase(frase, frasef);
+    printf("\nFrase final: ");
+    puts(frasef);
+}
+
+void retornaFrase(char frase[], char frasef[])
+{
+    char palavra[100];
+    int cont1=0, cont2=0, cont3=0;
+    while(frase[cont1] != '\0')
+    {
+        if(frase[cont1] != ' ')
+        {
+            palavra[cont2] = frase[cont1];
+            cont2++;
+            frasef[cont3] = frase[cont1];
+            cont3++;
+        }
+        else
+        {
+            frasef[cont3] = ' ';
+            cont3++;
+            for(int i=0; i<cont2; i++)
+            {
+                frasef[cont3] = palavra[i];
+                cont3++;
+            }
+            frasef[cont3]= ' ';
+            cont3++;
+            cont2=0;
+        }
+        cont1++;
+    }
+    frasef[cont3] = ' ';
+    cont3++;
+    for(int i=0; i<cont2; i++)
+    {
+        frasef[cont3] = palavra[i];
+        cont3++;
+    }
+    frasef[cont3] = '\0';
+}
